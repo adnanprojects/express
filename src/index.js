eea0bcc6c21f418e60eac7230bb6a97538eb5f76
adnanprojects <adnanchaudhary99@gmail.com>
2024-06-30 10:30:32 +0530

    Update App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import { query, validationResult } from 'express-validator';
-import { Router } from 'express';
+import cookieParser from 'cookie-parser';
 
 const app = express();
-
-const router = Router();
-
 app.use(express.json());
+app.use(cookieParser('sign'));
 
 const PORT = process.env.PORT || 3000;
 
@@ -26,7 +24,10 @@ const findIndexByUserId = (request, response, next) => {
     next();
 };
 
-app.get('/', (request, response) => response.status(201).send({ message: 'hello' }));
+app.get('/', (request, response) => {
+    response.cookie("name", "adnan", { maxAge: 60 * 1000 * 5, signed: true });
+    response.status(200).send({ message: 'Hello' })
+});
 
 app.get('/hello', query('person').notEmpty().escape(), (req, res) => {
     const result = validationResult(req);
@@ -39,13 +40,12 @@ app.get('/hello', query('person').notEmpty().escape(), (req, res) => {
 
 app.get('/api/users', query('filter').notEmpty(), (request, response) => {
     const result = validationResult(request);
-    if (result.isEmpty())
-        return response.send()
-    const { filter, value } = request.query;
-    if (filter && value) {
+    console.log(result.isEmpty());
+    if (result.isEmpty()) {
+        const { filter, value } = request.query;
         return response.send(mockUsers.filter(user => user[filter].includes(value)));
     };
-    return response.send(mockUsers);
+    return response.send({ error: result.array() });
     // TODO: Handle the case in for which filter is wrong and check for value, like: filter=specialname&value=adnan & filter=name&value=23 -> Use express-validator
 });
 
@@ -82,8 +82,13 @@ app.delete('/api/users/:id', (request, response) => {
     return response.sendStatus(200);
 });
 
-app.get('/api/products', (request, response) => response.send([
-    { id: 12, name: 'chicken tikka', price: 149 }
-]));
+app.get('/api/products', (request, response) => {
+    console.log(request.signedCookies);
+    if (request.signedCookies.name && request.signedCookies.name === 'adnan')
+        return response.send([
+            { id: 12, name: 'chicken tikka', price: 149 }
+        ]);
+    return response.send('Cant find this cookie');
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
