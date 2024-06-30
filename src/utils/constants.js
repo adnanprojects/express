2050b0a769ddf51ff0993013956f2c78cc983f0a
adnanprojects <adnanchaudhary99@gmail.com>
2024-06-30 21:53:43 +0530

    Add constant file

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const mockUsers = [
+    { id: 1, username: 'adnan', password: 'addu123' },
+    { id: 2, username: 'zaid', password: 'zaddu123' },
+    { id: 3, username: 'kashan', password: 'kaddu123' },
+];
