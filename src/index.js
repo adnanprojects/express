8a71025cf031035b046b2fa6552b79a7cc801322
adnanprojects <adnanchaudhary99@gmail.com>
2024-07-02 21:41:06 +0530

    Update App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import express from 'express';
+import express, { response } from 'express';
 import { query, validationResult } from 'express-validator';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser('sign'));
-
-const PORT = process.env.PORT || 3000;
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: false,
+    resave: false,
+    cookie: { maxAge: 1000 * 60 * 60 }
+}));
 
 const mockUsers = [
-    { id: 1, name: 'adnan' },
-    { id: 2, name: 'zaid' },
-    { id: 3, name: 'kashan' },
+    { id: 1, username: 'adnan', password: 'addu123' },
+    { id: 2, username: 'zaid', password: 'zaddu123' },
+    { id: 3, username: 'kashan', password: 'kaddu123' },
 ];
 
 const findIndexByUserId = (request, response, next) => {
@@ -25,8 +30,44 @@ const findIndexByUserId = (request, response, next) => {
 };
 
 app.get('/', (request, response) => {
+    request.session.visited = true;
     response.cookie("name", "adnan", { maxAge: 60 * 1000 * 5, signed: true });
     response.status(200).send({ message: 'Hello' })
+});
+
+// map session to single user
+app.post('/api/auth', (request, response) => {
+    const { body: { username, password } } = request;
+    const findUser = mockUsers.find(user => user.username = username);
+    if (!findUser || findUser.password !== password) return response.status(401).send({ message: 'BAD CREDENTIALS' });
+    request.session.user = findUser;
+    return response.status(200).send(findUser);
+});
+
+app.get('/api/auth/status', (request, response) => {
+    request.sessionStore.get(request.sessionID, (error, session) => {
+        console.log(session);
+    });
+    return request.session.user
+        ? response.status(201).send(request.session.user)
+        : response.status(401).send({ message: 'Not Authenticated' });
+});
+
+app.post('/api/cart', (request, response) => {
+    if (!request.session.user) return response.sendStatus(401);
+    const { body: item } = request;
+    const { cart } = request.session;
+    if (!cart) {
+        request.session.cart = [item];
+    } else {
+        cart.push(item);
+    }
+    return response.status(201).send(item);
+});
+
+app.get('/api/cart', (request, response) => {
+    if (!request.session.user) return response.sendStatus(401);
+    return response.send(request.session.cart ?? []);
 });
 
 app.get('/hello', query('person').notEmpty().escape(), (req, res) => {
@@ -34,7 +75,6 @@ app.get('/hello', query('person').notEmpty().escape(), (req, res) => {
     if (result.isEmpty()) {
         return res.send(`Hello, ${req.query.person}!`);
     }
-
     res.send({ errors: result.array() });
 });
 
@@ -83,7 +123,6 @@ app.delete('/api/users/:id', (request, response) => {
 });
 
 app.get('/api/products', (request, response) => {
-    console.log(request.signedCookies);
     if (request.signedCookies.name && request.signedCookies.name === 'adnan')
         return response.send([
             { id: 12, name: 'chicken tikka', price: 149 }
@@ -91,4 +130,5 @@ app.get('/api/products', (request, response) => {
     return response.send('Cant find this cookie');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
